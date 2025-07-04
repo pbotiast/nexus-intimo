8d1b7ab14f1f91c9d0383cfbd5d8e4f54e8b94d5
pbotias <pedrobotias@gmail.com>
2025-07-04 13:14:59 +0200

    fix: new design

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -200,3 +200,9 @@ export const DocumentDuplicateIcon: React.FC<React.SVGProps<SVGSVGElement>> = (p
     </svg>
 );  
 
+export const MapPinIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2} {...props}>
+        <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
+        <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
+    </svg>
+);
