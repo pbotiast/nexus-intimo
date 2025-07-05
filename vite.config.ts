38cfbdeaeab1543ced6e4d0873fb4cbfcfe9a494
pbotiast <75826282+pbotiast@users.noreply.github.com>
2025-07-05 08:30:56 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path'; // Importar el módulo 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      // Configura los alias para que Vite los resuelva durante la construcción
-      '@src': path.resolve(__dirname, './src'),
-      '@components': path.resolve(__dirname, './src/components'),
-      '@contexts': path.resolve(__dirname, './src/contexts'),
-      '@/views': path.resolve(__dirname, './views'), // Alias actualizado para el directorio de vistas
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001', // Servidor de desarrollo del backend
+        changeOrigin: true,
+      },
     },
   },
+  // --- AÑADIDO PARA FORZAR LA RECONSTRUCCIÓN ---
+  // Esto le dice a Vite que no guarde en caché las dependencias pre-compiladas.
+  optimizeDeps: {
+    force: true, 
+  },
 });
-  
